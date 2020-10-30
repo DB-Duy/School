1967b513e8172fe562dc3cfdbd78144b6b89bc74
DBDuy <dbduy2001@gmail.com>
2020-10-30 15:09:39 +0700

    fucked up

--- src/e2.java
+++ src/e2.java
@@ -1,29 +1,38 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.awt.*;
 import javax.swing.*;
 import java.util.Scanner;
 public class e2 {
-    public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        System.out.println("Enter frame width: ");
-        MyFrame.width=in.nextInt();
-        System.out.println("Enter frame height: ");
-        MyFrame.height=in.nextInt();
-        MyFrame frame = new MyFrame();
-        Dimension d=new Dimension(MyFrame.width,MyFrame.height);
-        draw e2= new draw();
-        e2.setPreferredSize(d);
-        frame.add(e2);
-        frame.pack();
+    public static void run() {
+        draw.start();
     }
-
     public static class draw extends JPanel{
+        public static int width=800;
+        public static int height=800;
+        public static JFrame frame = new JFrame();
+        public static void start(){
+            /*Scanner in=new Scanner(System.in);
+            System.out.println("Enter width: ");
+            width=in.nextInt();
+            System.out.println("Enter height: ");
+            height=in.nextInt();*/
+            draw e2=new draw();
+            frame.setPreferredSize(new Dimension(width,height));
+            e2.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()));
+            frame.getContentPane().add(e2);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
         public void paintComponent(Graphics g){
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g;
-            g2d.drawLine(0,0,MyFrame.width,MyFrame.height);
-            g2d.drawLine(0,MyFrame.height,MyFrame.width,0);
-            g2d.drawLine(0,MyFrame.height/2,MyFrame.width,MyFrame.height/2);
-            g2d.drawLine(MyFrame.width/2,0,MyFrame.width/2,MyFrame.height);
+                g.drawLine(0, 0, this.getWidth() , this.getHeight());
+                g.drawLine(0, this.getHeight(), this.getWidth(), 0);
+                g.drawLine(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
+                g.drawLine(this.getWidth() / 2, 0, this.getWidth() / 2, this.getHeight());
         }
+        
 }
 }
--- src/e3.java
+++ src/e3.java
@@ -3,27 +3,44 @@ import java.awt.*;
 import java.util.Scanner;
 
 public class e3 {
-    public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        System.out.println("Enter frame width: ");
-        MyFrame.width=in.nextInt();
-        System.out.println("Enter frame height: ");
-        MyFrame.height=in.nextInt();
-        MyFrame frame= new MyFrame();
-        Dimension d=new Dimension(MyFrame.width,MyFrame.height);
-        draw e3=new draw();
-        e3.setPreferredSize(d);
-        frame.add(e3);
-        frame.pack();
+    public static String c="b";
+    public static void run(){
+        draw.start();
     }
     public static class draw extends JPanel {
+        public static int width=800;
+        public static int height=800;
+        public static JFrame frame = new JFrame();
+        
+        public static void start(){
+            Scanner in = new Scanner(System.in);
+            /*System.out.println("Exercise 3 a or b: ");
+            c=in.nextLine();
+            System.out.println("Enter width: ");
+            width=in.nextInt();
+            System.out.println("Enter height: ");
+            height=in.nextInt();*/
+            draw e3=new draw();
+            frame.setPreferredSize(new Dimension(width,height));
+            e3.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()));
+            frame.getContentPane().add(e3);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g;
             for(int i=0;i<15;i++){
-                double x=-((double)MyFrame.width/(double)MyFrame.height)*i*((double)MyFrame.width/15)+(double)MyFrame.width;
-                double y=i*((double)MyFrame.width/15);
-                g.drawLine(0,0,(int)x,(int)y);
+                double j=frame.getContentPane().getWidth()/(double)frame.getContentPane().getHeight();
+                double y=i*((double)frame.getContentPane().getWidth()/15);
+                double x=-j*y+(double)frame.getContentPane().getHeight();
+                g.drawLine(0,0,(int)y,(int)x);
+                if(c.equalsIgnoreCase("b")){
+                    g.drawLine(this.getWidth(),this.getHeight(),(int)y,(int)x);
+                    g.drawLine(this.getWidth(),0,(int)x,(int)x);
+                    g.drawLine(0,this.getHeight(),(int)x,(int)x);
+                }
             }
         }
     }
